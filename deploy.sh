d02f7c1f7722f90d8099859e4718125488b32210
Jun Zheng <me@jackzh.com>
2018-11-24 11:42:06 -0500

    publish: Add basic footer with contact links
    generated from commit c20f436681c527aaf8337e02806add1375603532

--- deploy.sh
+++ deploy.sh
@@ -19,9 +19,12 @@ Options:
       --push-only          Only push but not build
 "
 
+BUILD_HASH=`git rev-parse --short HEAD`
+BUILD_TIME=`date +_%Y%m%d_%H%M%S`
+JEKYLL_ENV=$1 BUILD="$BUILD_HASH$BUILD_TIME" bundle exec jekyll serve
 
 run_build() {
-  bundle exec jekyll build
+  JEKYLL_ENV=$1 BUILD="$BUILD_HASH$BUILD_TIME" bundle exec jekyll build
 }
 
 parse_args() {
