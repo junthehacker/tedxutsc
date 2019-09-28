58a051b7131e6ded082ab47498046b8ce82b05e0
Jason Ho <jho70@myseneca.ca>
2019-09-28 00:34:49 -0400

    added validation for textarea and removed it for social media

--- assets/js/form/associateApplication.js
+++ assets/js/form/associateApplication.js
@@ -43,7 +43,9 @@ function validateForm() {
 
   var x, y, i, valid = true;
   x = document.getElementsByClassName("tab");
-  y = x[currentTab].getElementsByTagName("input");
+  //y = x[currentTab].getElementsByTagName("input");
+  //y = x[currentTab].getElementsByClassName("application");
+  y = x[currentTab].querySelectorAll("input, textarea");
 
   for (i = 0; i < y.length; i++) {
 
@@ -58,7 +60,11 @@ function validateForm() {
   if (valid) {
     document.getElementsByClassName("step")[currentTab].className += " finish";
   }
-  return valid; 
+
+  if(currentTab == 3) {
+    return true;
+  }
+  return valid;
 }
 
 function fixStepIndicator(n) {
