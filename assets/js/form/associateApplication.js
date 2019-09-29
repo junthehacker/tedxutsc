054d279488fe2329e8a8d450ffd8dc04519482f4
Jason Ho <jho70@myseneca.ca>
2019-09-29 16:28:59 -0400

    publish: changed button on home page
    generated from commit 1c4540e74c11f2317f46820a11b9b923ea4dfe4c

--- assets/js/form/associateApplication.js
+++ assets/js/form/associateApplication.js
@@ -3,6 +3,41 @@ var scriptURL = "https://script.google.com/macros/s/AKfycbxq0GIXDVKGZc09Auk_RakC
 const form = document.forms['submit-to-google-sheet'];
 showTab(currentTab); 
 
+$('#resume').on('change', function() {
+
+  var file = this.files[0];
+  var fr = new FileReader();
+  fr.fileName = file.name;
+  fr.onload = function(e) {
+    e.target.result;
+    html = '<input type="hidden" name="data" value="' + e.target.result.replace(/^.*,/, '') + '" >';
+    html += '<input type="hidden" name="mimetype" value="' + e.target.result.match(/^.*(?=;)/)[0] + '" >';
+    html += '<input type="hidden" name="filename" value="' + e.target.fileName + '" >';
+    $("#data").empty().append(html);
+  }
+  fr.readAsDataURL(file);
+});
+
+form.addEventListener("submit", () => {
+  span = document.getElementById("span");
+  nextBtn = document.getElementById("nextBtn");
+  prevBtn = document.getElementById("prevBtn");
+
+  span.style.display="none";
+  nextBtn.style.display="none";
+  prevBtn.style.display="none";
+  console.log("submit clicked")
+})
+
+function showMessage(e){
+
+  $('#progress').html(e);
+}
+
+function hideMessage() {
+  $('.' + $('.close').attr('data-hide')).hide();
+}
+
 function showTab(n) {
   
   var x = document.getElementsByClassName("tab");
@@ -33,10 +68,33 @@ function nextPrev(n) {
   currentTab = currentTab + n;
 
   if (currentTab >= x.length) {
-    fetch(scriptURL, {method: 'POST', body: new FormData(form)})
-      .then(Response => console.log('Success!', response))
-      .catch(error => console.error('Error!', error.message))
-    document.getElementById("regForm").submit();
+      
+      var data = new FormData();
+      data.append('resume', document.getElementById('resume').files);
+
+      fetch(scriptURL, {method: 'POST', body: data })
+      .then(response => {
+        console.log('Success!', response)
+        span = document.getElementById("span");
+        nextBtn = document.getElementById("nextBtn");
+        prevBtn = document.getElementById("prevBtn");
+
+        span.style.display="none";
+        nextBtn.style.display="none";
+        prevBtn.style.display="none";
+
+        $("#success-message").attr("class", "alert alert-success alert-dismissible");
+        document.getElementById("message").innerText = "Thank you very much for your application! Please note that only successful candidates will be contacted";
+        $('.alert').show();
+      })
+      .catch(error => {
+        console.error('Error!', error.message)
+        $("#success-message").attr("class", "alert alert-danger alert-dismissible");
+        document.getElementById("message").innerText = "Error! Something went wrong";
+        $('.alert').show();
+      })
+
+      document.getElementById("regForm").submit();
     
     return false;
   }
